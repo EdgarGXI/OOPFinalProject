1ec84261ee49a087d8cc558473bcd0a0b849d07a
JeanCDV <98917687+JeanCDV@users.noreply.github.com>
2022-04-21 08:45:08 -0500

    Update Implementation.py

--- Project/Implementation.py
+++ Project/Implementation.py
@@ -1,8 +1,16 @@
 from Classes import *
+import folium
 
+##Creates map
+map = folium.Map(location=[11.019033, -74.850760], zoom_start=14, control_scale=True)
+
+map
+
+##Asks for user input
 r=Input
 r.getInput()
 
+##Locates the file
 a=File
 a.getFile("text.txt")
 
