82880196bdd2391e675daa3bdc0ac63543862291
JeanCDV <98917687+JeanCDV@users.noreply.github.com>
2022-05-22 13:20:53 -0500

    Update Implementation.py

--- Project/Implementation.py
+++ Project/Implementation.py
@@ -1,16 +1,43 @@
-from Classes import *
-import folium
+import tkintermapview
+from tkinter import *
+##from tkinter import tkintermapview
+from tkinter import simpledialog
 
-##Creates map
-map = folium.Map(location=[11.019033, -74.850760], zoom_start=14, control_scale=True)
+root= Tk()
+##root.configure(bg="gray")
+root.title("Mapa - Universidad del norte")
+root.geometry("900x700")
+esp3 = Label(root, text=" ")
+esp3.pack()
+title = Label(root, text = "CompaniUN", font=("Calibri", 20))
+title.pack()
+##user = simpledialog.askstring(title = "CompaniUN", prompt = "Indique un lugar:")
 
-map
+my_label= LabelFrame(root)
+my_label.pack(pady=20)
 
-##Asks for user input
-r=Input
-r.getInput()
+map_widget = tkintermapview.TkinterMapView(my_label, width=800, height=500,corner_radius=2)
+map_widget.set_position(11.018985992247588, -74.85068279601066)
+map_widget.pack()
+map_widget.set_zoom(20)
 
-##Locates the file
-a=File
-a.getFile("text.txt")
+
+esp1 = Label(root, text = " ")
+esp2 = Label(root, text = " ")
+ask = Label(root, text = "Indique hacia donde quiere ir: ", font=("Arial", 14))
+user2 = Entry(root, width=50)
+ask.pack()
+esp1.pack()
+user2.pack()
+
+
+def myClick():
+    boton=Label(root, text="Escribiste: " + user2.get()) 
+    boton.pack()
+
+myButton = Button(root, text="Buscar", command=myClick)
+myButton.pack()
+
+
+root.mainloop()
 
